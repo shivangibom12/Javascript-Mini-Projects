efb773975b6b47b27ee1b899b855aa9e3f7355d2
SHIVANGI BAM <bamshivangi@gmail.com>
2020-08-30 16:27:28 +0530

    Bulb ON-OFF

---
+++ Bulb ON-OFF/js/script.js
@@ -0,0 +1,8 @@
+ function clickMeOn() {
+	document.getElementById('bulb').src='images/pic_bulbon.gif';
+	 
+}
+
+function clickMeOff() {
+	document.getElementById('bulb').src='images/pic_bulboff.gif';
+}
